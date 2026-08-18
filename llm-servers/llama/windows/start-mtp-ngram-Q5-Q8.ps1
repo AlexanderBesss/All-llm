9127588a2929155766b90995a72cd37c604c334f
Oleksandr Besarab <alexandrbesss@gmail.com>
2026-08-18 11:33:44 +0300

    feat: update ngram processing scripts with new cache types and add additional configurations

--- llm-servers/llama/windows/start-mtp-ngram-Q5-Q8.ps1
+++ llm-servers/llama/windows/start-mtp-ngram-Q5-Q8.ps1
@@ -1,5 +1,5 @@
 & (Join-Path $PSScriptRoot 'llama\llama-server.exe') `
-  -m "..\..\..\models\unsloth\Qwen3.8-27B-GGUF\Qwen3.8-27B-UD-Q4_K_XL.gguf" `
+  -m "..\..\..\models\unsloth\Qwen3.8-27B-GGUF\Qwen3.8-27B-UD-Q5_K_XL.gguf" `
   --port 8080 `
   --host 0.0.0.0 `
   --gpu-layers all `
@@ -9,9 +9,9 @@
   --gpu-layers-draft all `
   --parallel 1 `
   --cache-ram 0 `
-  --ctx-size 120000 `
-  --cache-type-k q4_0 `
-  --cache-type-v q4_0 `
+  --ctx-size 80000 `
+  --cache-type-k q8_0 `
+  --cache-type-v q8_0 `
   --flash-attn on `
   --batch-size 2048 `
   --ubatch-size 1024 `
